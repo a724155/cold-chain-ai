96339ee42317a074d62ec14ce952b45d2c2adefd
a724155 <724155616@qq.com>
2026-07-16 18:03:29 +0800

    1

---
+++ src/main/java/com/ymm/coldchainai/ColdChainAiApplication.java
@@ -0,0 +1,13 @@
+package com.ymm.coldchainai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdChainAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdChainAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ymm/coldchainai/ColdChainAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymm.coldchainai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdChainAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
